376c53429932fd6c8c9abf7e772130158e281447
Lucas <lucas.t.w.desenvolvedor@gmail.com>
2025-10-07 21:53:34 -0300

    primeira pagina

--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,7 @@ import { useState } from 'react';
 export {useState} from 'react';
 
 function home (){
-    return (<div>
-        <h1>Home</h1>
-        <Contador />
-        </div>
-    )
-    
+    return<h1>Home</h1>
+
 }
-function Contador (){
-
-    const [contador , setContador] = useState(1);
-
-     function addContador(){
-  setContador(contador+1);
-     }
-
-     return (
-     <div>
-        <div>{contador}</div>
-     <button onClick={addContador}>adicionar</button>
-     </div>
-     )
-}
-
 export default home;
