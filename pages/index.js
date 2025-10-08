ab2950f2206d750c9754ba3be15c5c6b15308ade
Lucas <lucas.t.w.desenvolvedor@gmail.com>
2025-10-07 22:08:33 -0300

    pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
-import { useState } from 'react';
-
-export {useState} from 'react';
+import { useState } from "react";
 
 function home (){
-    return<h1>Home</h1>
+    return(<div><h1>Home</h1>
+    <Numero /></div>
+    )
+
+    function Numero (){
+ const[contador , setContador] = useState(1);
+ function contar (){
+     return(
+        <div>
+        <div>
+            {contador}
+        </div>
+        <button onClick ={setContador}></button>
+        </div>
+     )
+ }
+    }
 
 }
 export default home;
